316b39785fc1754fd28372f8f4404b6776586075
Rifky Galuh <rfqgal@users.noreply.github.com>
2022-02-08 16:55:09 +0700

    first commit

---
+++ database/migrations/2022_02_08_093025_create_sales_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('sale_id');
+            $table->string('product_code');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_details');
+    }
+}
