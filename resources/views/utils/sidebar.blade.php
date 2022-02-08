f074009789f2df6db03cc13c20327879bc4cebbb
Rifky Galuh <rfqgal@users.noreply.github.com>
2022-02-09 04:02:38 +0700

    fix(sidebar): changed sidebar to fixed-top

--- resources/views/utils/sidebar.blade.php
+++ resources/views/utils/sidebar.blade.php
@@ -1,4 +1,26 @@
-<nav class="col-2 h-100 bg-dark text-white py-2 px-5">
+<nav class="fixed-top col-2 h-100 bg-dark text-white py-2 px-5">
+  <div class="py-2 mx-auto">
+    <a class="fs-2" href="/">RG Comps</a>
+  </div>
+  <div class="d-grid gap-3 px-3 py-2">
+    <div>
+      <a href="/">Dashboard</a>
+    </div>
+    <div>
+      <a href="/products">Products</a>
+    </div>
+    <div>
+      <a href="/sales">Sales</a>
+    </div>
+  </div>
+  <div class="fixed-bottom py-2 px-5">
+    <div class="px-3 py-3">
+      <a href="#">Logout</a>
+    </div>
+  </div>
+</nav>
+
+<nav class="hidden col-2 h-100 bg-dark text-white py-2 px-5">
   <div class="py-2 mx-auto">
     <a class="fs-2" href="/">RG Comps</a>
   </div>
@@ -11,11 +33,6 @@
     </div>
     <div>
       <a href="/sales">Sales</a>
-    </div>
-  </div>
-  <div class="fixed-bottom py-2 px-5">
-    <div class="px-2 py-3">
-      <a href="#">Logout</a>
     </div>
   </div>
 </nav>
